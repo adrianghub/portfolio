e2cbfe872233ad9f8883db015640920a5e87f45f
Adrian Zinko <adrian.zinko@boldare.com>
2022-06-11 09:20:08 +0200

    feat(): add color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,30 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: "media",
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        button: {
+          wenge: {
+            100: '#f5f0f2',
+            200: '#e0d2d8',
+            300: '#cbb3be',
+            400: '#b695a3',
+            500: '#a27789',
+            600: '#885d70',
+            700: '#6a4957',
+            800: '#2d1f25',
+            900: '#2d1f25'
+          }
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
