0adf09cddde435265fc72f60aec3cf57142d9c32
Adrian Zinko <adrian.zinko@boldare.com>
2022-06-15 21:37:18 +0200

    feat(): add document file

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-whitechocolate-300 h-screen w-full">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
