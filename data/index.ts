47a6da7667c6cfbae556c842f93c296cdd3e204a
Adrian Zinko <adrian.zinko@boldare.com>
2022-07-29 08:39:24 +0200

    fix(): correct social media url

--- data/index.ts
+++ data/index.ts
@@ -2,6 +2,6 @@ export const SOCIAL_MEDIA = {
   GH_HREF: 'https://github.com/adrianghub',
   LINKEDIN_HREF: 'https://www.linkedin.com/in/adrian-zinko/',
   INSTA_HREF: 'https://www.instagram.com/_drianko/',
-  TWITTER_HREF: 'https://twitter.com/drianko',
+  TWITTER_HREF: 'https://twitter.com/AdrianZinko',
   EMAIL_HREF: 'mailto:zinko.adrian00@gmail.com'
 };
