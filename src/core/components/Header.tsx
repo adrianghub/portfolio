a7b6639abd2577873436f5d46138095189079014
Adrian Zinko <adrian.zinko@boldare.com>
2023-03-05 12:20:11 +0100

    imp: add logo

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -2,11 +2,17 @@
 
 import { ThemeToggle } from '@/app/theme-toggle';
 import { SearchSpotlight } from '@/modules/blog/components/SearchSpotlight';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+import logoDark from '../../../public/assets/icons/logo-dark.svg';
+import logoLight from '../../../public/assets/icons/logo-light.svg';
+
 export const Header = () => {
   const pathname = usePathname();
+  const { resolvedTheme } = useTheme();
 
   const stickyHeaderPath = pathname === '/';
 
@@ -16,10 +22,15 @@ export const Header = () => {
         stickyHeaderPath ? 'sticky top-0 backdrop-blur-sm' : ''
       }`}
     >
-      <div className="border-b w-full border-gray-300 py-8 flex flex-col justify-between items-center sm:flex-row">
+      <div className="border-b w-full border-gray-300 pt-8 flex flex-col justify-between items-center sm:flex-row">
         <div className="flex items-center py-4">
           <Link href="/" className="site-title text-3xl lg:text-5xl mr-2">
-            portfolio
+            <Image
+              src={resolvedTheme === 'light' ? logoDark : logoLight}
+              alt="Portoflio logo with author name."
+              width={200}
+              height={200}
+            />
           </Link>
           <ThemeToggle />
         </div>
