c45fce377225e95cf850231cec18fdb5c4434b87
Adrian Zinko <zinko.adrian00@gmail.com>
2023-06-05 17:50:08 +0200

    chore: update to next13.4 (stable app router)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from './theme-provider';
 
 export const revalidate = 60;
 
-const inconsolata = Inconsolata();
+const inconsolata = Inconsolata({ subsets: ['latin'] });
 
 export default function RootLayout({
   children
